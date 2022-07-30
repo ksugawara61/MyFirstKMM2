031781c56a498705b6ff528872a1d49408b639ae
ksugawara61 <ksugawara61@users.noreply.github.com>
2022-07-30 21:46:55 +0900

    fix

--- androidApp/src/main/java/net/listadoko/myfirstkmm2/android/MainActivity.kt
+++ androidApp/src/main/java/net/listadoko/myfirstkmm2/android/MainActivity.kt
@@ -2,7 +2,6 @@ package net.listadoko.myfirstkmm2.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -13,12 +12,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
 import net.listadoko.myfirstkmm2.Greeting
-
-fun greet(): String {
-    return Greeting().greeting()
-}
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,15 +21,19 @@ class MainActivity : AppCompatActivity() {
 
         val greeting = findViewById<ComposeView>(R.id.greeting)
         greeting.setContent {
-            GreetingText()
+            GreetingText(greet())
         }
+    }
+
+    private fun greet(): String {
+        return Greeting().greeting()
     }
 }
 
 @Composable
-private fun GreetingText() {
+private fun GreetingText(text: String) {
     Text(
-        text = stringResource(id = R.string.greeting),
+        text = text,
         style = MaterialTheme.typography.h5,
         modifier = Modifier
             .fillMaxWidth()
