914be849992b81bd3c5544942f31a778b8a4ee2b
ksugawara61 <ksugawara61@users.noreply.github.com>
2022-07-30 21:05:34 +0900

    Add: jetpack compose

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,6 +17,13 @@ android {
             isMinifyEnabled = false
         }
     }
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.1"
+    }
 }
 
 dependencies {
@@ -24,4 +31,10 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.compose.material:material:1.2.0")
+    implementation("androidx.compose.animation:animation:1.2.0")
+    implementation("androidx.compose.ui:ui-tooling:1.2.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.2.0")
 }
--- androidApp/src/main/java/net/listadoko/myfirstkmm2/android/MainActivity.kt
+++ androidApp/src/main/java/net/listadoko/myfirstkmm2/android/MainActivity.kt
@@ -2,8 +2,19 @@ package net.listadoko.myfirstkmm2.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import net.listadoko.myfirstkmm2.Greeting
 import android.widget.TextView
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import net.listadoko.myfirstkmm2.Greeting
 
 fun greet(): String {
     return Greeting().greeting()
@@ -14,7 +25,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        val greeting = findViewById<ComposeView>(R.id.greeting)
+        greeting.setContent {
+            GreetingText()
+        }
     }
+}
+
+@Composable
+private fun GreetingText() {
+    Text(
+        text = stringResource(id = R.string.greeting),
+        style = MaterialTheme.typography.h5,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
 }
