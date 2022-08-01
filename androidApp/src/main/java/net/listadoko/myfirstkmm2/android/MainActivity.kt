9f49a5fb690f178fcd67115e4d0f23d62492ec30
ksugawara61 <ksugawara61@users.noreply.github.com>
2022-08-01 22:34:48 +0900

    Androidのカウンターを追加

--- androidApp/src/main/java/net/listadoko/myfirstkmm2/android/MainActivity.kt
+++ androidApp/src/main/java/net/listadoko/myfirstkmm2/android/MainActivity.kt
@@ -2,42 +2,76 @@ package net.listadoko.myfirstkmm2.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
-import androidx.compose.material.MaterialTheme
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.ComposeView
-import androidx.compose.ui.res.dimensionResource
-import net.listadoko.myfirstkmm2.Greeting
+import androidx.compose.ui.text.AnnotatedString
+import com.arkivanov.essenty.instancekeeper.instanceKeeper
+import com.arkivanov.essenty.lifecycle.essentyLifecycle
+import com.arkivanov.mvikotlin.core.view.BaseMviView
+import com.arkivanov.mvikotlin.logging.store.LoggingStoreFactory
+import com.arkivanov.mvikotlin.main.store.DefaultStoreFactory
+import net.listadoko.myfirstkmm2.feature.counter.CalculatorController
+import net.listadoko.myfirstkmm2.feature.counter.CalculatorView
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var proxy: CalculatorViewImpl
+    private lateinit var controller: CalculatorController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val greeting = findViewById<ComposeView>(R.id.greeting)
-        greeting.setContent {
-            GreetingText(greet())
+        val lifecycle = essentyLifecycle()
+        proxy = CalculatorViewImpl()
+        controller = CalculatorController(
+            storeFactory = LoggingStoreFactory(delegate = DefaultStoreFactory()),
+            instanceKeeper = instanceKeeper(),
+            lifecycle = lifecycle
+        )
+        controller.onViewCreated(proxy, lifecycle)
+
+        setContent {
+            Body()
         }
     }
 
-    private fun greet(): String {
-        return Greeting().greeting()
+    @Composable
+    private fun Body() {
+        val model by proxy.model.asStateFlow().collectAsState()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Text(
+                text = AnnotatedString(model.value)
+            )
+            Button(onClick = { proxy.dispatch(CalculatorView.Event.IncrementClicked) }) {
+                Text("Increment")
+            }
+            Button(onClick = { proxy.dispatch(CalculatorView.Event.DecrementClicked) }) {
+                Text("Decrement")
+            }
+            Button(onClick = { proxy.dispatch(CalculatorView.Event.SumClicked(n = 10)) }) {
+                Text("Sum")
+            }
+        }
     }
 }
 
-@Composable
-private fun GreetingText(text: String) {
-    Text(
-        text = text,
-        style = MaterialTheme.typography.h5,
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
-            .wrapContentWidth(Alignment.CenterHorizontally)
-    )
+private class CalculatorViewImpl: BaseMviView<CalculatorView.Model, CalculatorView.Event>(), CalculatorView {
+    val model = MutableStateFlow(CalculatorView.Model())
+
+    override fun render(model: CalculatorView.Model) {
+        this.model.update { it.copy(model.value) }
+    }
 }
